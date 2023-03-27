7fef2da34b3eb583ca99e4c852da4929a68b6489
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-27 04:43:39 -0700

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""task one starts flask web app"""
+
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hey_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def heybnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def weluvpy(text="is cool"):
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<n>')
+def number(n):
+    try:
+        n = int(n)
+        return "{} is a number".format(n)
+    except:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
