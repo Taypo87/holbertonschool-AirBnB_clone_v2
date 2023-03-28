8cce40833b42fa329d6e2cbc3874bd7028f8abc1
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-28 09:13:15 -0700

    fixed pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 # starts a webflask app
 
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
 @app.route('/states_list')
+"""states list"""
+
+
 def states_list():
     return render_template('7-states_list.html',
                            states=storage.all("State"))
 
 
 @app.teardown_appcontext
+"""tear down"""
+
+
 def teardown(err):
     storage.close()
 
